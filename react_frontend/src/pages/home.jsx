1dadfcbc98fd2842827b93197283cfbd3e9d6b8e
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-03 14:02:39 +0300

    Deleting unnecessary images and completing homepage

--- react_frontend/src/pages/home.jsx
+++ react_frontend/src/pages/home.jsx
@@ -17,10 +17,10 @@ const Home = () => {
 
             <div className='categories_wrapper'>
 
-            <House addClass="active" img_src="./components./test" name="Islands" />
-            <House img_src="./components./test" name="Beach" />
-            <House img_src="./components./test" name="Amazing pools" />
-            <House img_src="./components./test" name="OMG!" />
+            <House addClass="active" img_src="https://a0.muscache.com/pictures/8e507f16-4943-4be9-b707-59bd38d56309.jpg" name="Islands" />
+            <House img_src="https://a0.muscache.com/pictures/10ce1091-c854-40f3-a2fb-defc2995bcaf.jpg" name="Beach" />
+            <House img_src="https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg" name="Amazing pools" />
+            <House img_src="https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg" name="OMG!" />
 
             </div>
             <div className='houses_options'>
@@ -41,10 +41,34 @@ const Home = () => {
           </div>
 
           <div className='stays'>
-            <div className='stay'>
+            <div className='stays_wrapper'>
+              <div className='stay'>
 
-              <div className='stay_img'>
-                <img src="/src/assets/fa2ed873-e726-4e6d-ab1a-28bc7c69d805.webp" />
+                <div className='stay_img'>
+                  <img src="https://a0.muscache.com/im/pictures/fa2ed873-e726-4e6d-ab1a-28bc7c69d805.jpg?im_w=720" />
+                </div>
+
+              </div>
+              <div className='stay'>
+
+                <div className='stay_img'>
+                  <img src="https://a0.muscache.com/im/pictures/fa2ed873-e726-4e6d-ab1a-28bc7c69d805.jpg?im_w=720" />
+                </div>
+
+              </div>
+              <div className='stay'>
+
+                <div className='stay_img'>
+                  <img src="https://a0.muscache.com/im/pictures/fa2ed873-e726-4e6d-ab1a-28bc7c69d805.jpg?im_w=720" />
+                </div>
+
+              </div>
+              <div className='stay'>
+
+                <div className='stay_img'>
+                  <img src="https://a0.muscache.com/im/pictures/fa2ed873-e726-4e6d-ab1a-28bc7c69d805.jpg?im_w=720" />
+                </div>
+
               </div>
 
             </div>
